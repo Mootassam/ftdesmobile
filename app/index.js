0b753d3e33bf0a46302d89a7613c64c7c5016649
Mootassem <mootassem@tadeco-group.tn>
2022-11-06 13:46:09 -0500

    add

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Text>App.js and Im The best in the world</Text>
+    </SafeAreaView>
+  );
+}
