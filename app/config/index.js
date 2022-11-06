90f8ad35692ea3356b29984812ecf13e1aa92e02
Mootassem <mootassem@tadeco-group.tn>
2022-11-06 14:29:51 -0500

    Add color settigns

---
+++ app/config/index.js
@@ -0,0 +1,10 @@
+import {
+  BaseColor,
+  useTheme,
+  useFont,
+  FontSupport,
+  ThemeSupport,
+  DefaultFont,
+} from './theme';
+
+export {BaseColor, useTheme, useFont, FontSupport, ThemeSupport, DefaultFont};
---
+++ app/config/theme.js
@@ -0,0 +1,19 @@
+export const BaseColor = {
+  red: '#e1011a',
+  white: '#ffffff',
+  violet: '#2b2840',
+  violetdark: '#201e31',
+  /* Secodary Color */
+  background: '#f1f1f1',
+  gray1: '#e5e5e5',
+  gray2: '#4f4f4f',
+  gray3: '#828282',
+  gray4: '#f1f1f1',
+  gray5: '#a3a3a9',
+  gray6: '#f4f4f4',
+  navigate__left: '#bfbfc6',
+  darkred: '#c40720',
+  green: '#14b685',
+  border__green: '#8bdfc7',
+  background__green: '#eaf9f6',
+};
